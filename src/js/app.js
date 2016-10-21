c1f87d5cce397f0599fed629e4a3ef808810d7a6
unknown <>
2016-10-21 19:05:35 +0800

    add

--- src/js/app.js
+++ src/js/app.js
@@ -2,40 +2,43 @@
 var app = angular.module('myApp', ["ngRoute"])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
-            .when('/', {
+            .when('/personal', {
                 url: "/personal",
                 templateUrl: "../../html/personal.html",
                 controller: "personalCtrl"
             })
-            .when('/', {
+            .when('/basicSituation', {
                 url: "/basicSituation",
                 templateUrl: "../../html/basicSituation.html",
                 controller: "basicSituationCtrl"
             })
-            .when('/', {
+            .when('/medicalHistory', {
                 url: "/medicalHistory",
                 templateUrl: "../../html/medicalHistory.html",
                 controller: "medicalHistoryCtrl"
             })
-            .when('/', {
+            .when('/habitsCustoms', {
                 url: "/habitsCustoms",
                 templateUrl: "../../html/habitsCustoms.html",
                 controller: "habitsCustomsCtrl"
             })
-            .when('/', {
+            .when('/medication', {
                 url: "/medication",
                 templateUrl: "../../html/medication.html",
                 controller: "medicationCtrl"
             })
-            .when('/', {
+            .when('/breast', {
                 url: "/breast",
                 templateUrl: "../../html/breast.html",
                 controller: "breastCtrl"
             })
             .otherwise({
-                redirectTo: '/'
+                url: "/personal",
+                templateUrl: "../../html/personal.html",
+                controller: "personalCtrl"
             });
     }]);
+
 
 // 个人信息
 app.controller('personalCtrl', function ($scope, $http) {
@@ -55,6 +58,16 @@ app.controller('personalCtrl', function ($scope, $http) {
 // 基本情况
 app.controller('basicSituationCtrl', function ($scope, $http) {
     console.log("222");
+    var swiper = new Swiper('.swiper-container', {
+        direction: 'horizontal',
+        loop: true,
+        touchMoveStopPropagation: true, //阻止冒泡
+        paginationClickable: true,
+        pagination: '.swiper-pagination',
+        flip: {
+            slideShadows: false
+        },
+    });
 });
 
 // 疾病与家族史
